d97c81289ee29c621da40f1e9763e42a8b1e0cf1
praveen <praveentech847@gmail.com>
2020-06-29 16:25:16 +0530

    categories

--- src/components/Categories.vue
+++ src/components/Categories.vue
@@ -1,18 +1,34 @@
 <template>
   <section class="featured-categories">
     <b-container>
+      <div class="title-box">
+        <h2>Categories</h2>
+      </div>
       <b-row>
-        <b-col md="4">
+        <b-col md="2">
           <img
-            src="https://www.handletheheat.com/wp-content/uploads/2015/03/Best-Birthday-Cake-with-milk-chocolate-buttercream-SQUARE.jpg"
+            src="https://www.bigbasket.com/media/customPage/b01eee88-e6bc-410e-993c-dedd012cf04b/4ec5c320-719c-4c16-bbb4-5dc4be672239/23dbb0fa-291d-4436-a7e5-c2fb265f53e9/2007025_tea_360.jpg"
           />
         </b-col>
-        <b-col md="4">
-          <img src="https://5.imimg.com/data5/LM/DU/MY-22954806/apple-fruit-500x500.jpg" />
+        <b-col md="2">
+          <img src="https://www.bigbasket.com/media/customPage/b01eee88-e6bc-410e-993c-dedd012cf04b/4ec5c320-719c-4c16-bbb4-5dc4be672239/23dbb0fa-291d-4436-a7e5-c2fb265f53e9/2007026_coffee_360.jpg" />
         </b-col>
-        <b-col md="4">
+        <b-col md="2">
           <img
-            src="https://previews.123rf.com/images/utima/utima1311/utima131100146/24077398-orange-fruit-isolated-on-white-background.jpg"
+            src="https://www.bigbasket.com/media/customPage/b01eee88-e6bc-410e-993c-dedd012cf04b/4ec5c320-719c-4c16-bbb4-5dc4be672239/23dbb0fa-291d-4436-a7e5-c2fb265f53e9/2007027_health-drinks_360.jpg"
+          />
+        </b-col>
+        <b-col md="2">
+          <img
+            src="https://www.bigbasket.com/media/customPage/b01eee88-e6bc-410e-993c-dedd012cf04b/4ec5c320-719c-4c16-bbb4-5dc4be672239/23dbb0fa-291d-4436-a7e5-c2fb265f53e9/2007028_juices_360.jpg"
+          />
+        </b-col>
+        <b-col md="2">
+          <img src="https://www.bigbasket.com/media/customPage/b01eee88-e6bc-410e-993c-dedd012cf04b/4ec5c320-719c-4c16-bbb4-5dc4be672239/23dbb0fa-291d-4436-a7e5-c2fb265f53e9/2007029_cold-drinks_360.jpg" />
+        </b-col>
+        <b-col md="2">
+          <img
+            src="https://www.bigbasket.com/media/customPage/b01eee88-e6bc-410e-993c-dedd012cf04b/4ec5c320-719c-4c16-bbb4-5dc4be672239/23dbb0fa-291d-4436-a7e5-c2fb265f53e9/2007030_syrups-concentrate_360.jpg"
           />
         </b-col>
       </b-row>
